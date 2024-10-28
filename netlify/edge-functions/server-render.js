4a57b6045b65755264b52dc093f735ee2252a65b
Xiomy Castillo <xiomyp1>
2024-10-27 23:56:47 -0500

    fix: eliminacion de edge para pruebas

--- netlify/edge-functions/server-render.js
+++ netlify/edge-functions/server-render.js
@@ -1,6 +1,5 @@
 export default async (request, context) => {
-    return new Response("Edge function ejecutada correctamente.", {
-      headers: { "content-type": "text/plain" },
-    });
-  };
-  
+  return new Response("Edge function ejecutada correctamente.", {
+    headers: { "content-type": "text/plain" },
+  });
+};
